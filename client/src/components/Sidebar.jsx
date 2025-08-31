dfa1e98f831b0413b004700b4c5f4b4fe44c3290
anuj9631 <anuj98426@gmail.com>
2025-08-31 13:31:44 +0530

    Sidebar template completed

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,13 +3,13 @@ import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import moment from "moment";
 
-const Sidebar = () => {
+const Sidebar = ({isMenuOpen, setIsMenuOpen}) => {
   const { chats, setSelectedChats, theme, setTheme, user, navigate } =
     useAppContext();
   const [search, setSearch] = useState("");
 
   return (
-    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-10">
+    <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
       {/* Logo */}
       <img
         src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
@@ -48,9 +48,9 @@ const Sidebar = () => {
               .includes(search.toLowerCase())
           )
           .map((chat) => (
-            <div
+            <div onClickCapture={()=>{navigate('/'); setSelectedChats(chat), setIsMenuOpen(false)}}
               key={chat._id}
-              onClick={() => setSelectedChats(chat)}
+              // onClick={() => setSelectedChats(chat)}
               className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
             >
               <div>
@@ -75,7 +75,7 @@ const Sidebar = () => {
       {/* Community*/}
       <div
         onClick={() => {
-          navigate("/Community");
+          navigate("/Community"); setIsMenuOpen(false)
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -93,7 +93,7 @@ const Sidebar = () => {
        {/* Credit Purchase option */}
 <div
   onClick={() => {
-    navigate("/credits");
+    navigate("/credits");setIsMenuOpen(false);
   }}
   className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
 >
@@ -114,7 +114,7 @@ const Sidebar = () => {
           <p>Dark Mode</p>
         </div>
         <label className="relative inline-flex cursor-pointer">
-          <input onClick={()=> setTheme(theme === 'dark' ? 'light' : 'dark')} type="checkbox" className="sr-only peer" checked={theme === 'dark'}/>
+          <input onChange={()=> setTheme(theme === 'dark' ? 'light' : 'dark')} type="checkbox" className="sr-only peer" checked={theme === 'dark'}/>
           <div className='w-9 h-5 bg-gray-400 rounded-full peer-checked:bg-purple-600 transition-all'>
           </div>
           <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4">
@@ -122,6 +122,23 @@ const Sidebar = () => {
           </span>
         </label>
       </div>
+
+       {/* User Account*/} 
+
+<div
+        className="flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer group"
+      >
+        <img
+          src={assets.user_icon}
+          className="w-4.5 rounded-full"
+          alt=""
+        />
+          <p className="flex-1 text-sm dark:text-primary truncate">{user ? user.name : 'Login your account'}</p>
+        {user && <img src={assets.logout_icon} className="h-5  cursor-pointer hidden not-dark:invert group-hover:block" />}
+      </div>
+
+<img onClick={()=>setIsMenuOpen(false)} src={assets.close_icon} className="absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert" alt="" />
+
     </div>
   );
 };
