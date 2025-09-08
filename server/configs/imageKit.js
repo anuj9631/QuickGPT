cbf021a9a7723fb46c8348cf5db885ef1cd4cd52
anuj9631 <anuj98426@gmail.com>
2025-09-08 13:53:39 +0530

    image and text generation through api

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,12 +1,13 @@
 import ImageKit from "imagekit";
 
 
-var ImageKit = require("imagekit");
+// var ImageKit = require("imagekit");
 
 var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : IMAGEKIT_URL_ENDPOINT
+    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 export default imagekit
+
--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -3,10 +3,18 @@ import axios from "axios"
 import Chat from "../models/chat.js";
 import User from "../models/user.js";
 import imagekit from "../configs/imageKit.js";
+import openai from '../configs/openai.js'
 
 export const textMessageController = async (req, res) => {
   try {
     const userId = req.user._id;
+
+    
+    if(req.user.credits <1 ){
+      return res.json({success : false, message: "You don't have enough credits to use this features"})
+    }
+
+
     const { chatId, prompt } = req.body;
 
     const chat = await Chat.findOne({ userId, _id: chatId });
@@ -35,7 +43,7 @@ export const textMessageController = async (req, res) => {
     };
 
     res.json({ success: true, reply });
-    chat.message.push(reply);
+    chat.messages.push(reply);
     await chat.save();
 
     await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
@@ -79,7 +87,10 @@ export const imageMessageController = async (req, res) => {
 
    //convert base64
 
-   const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data,"binary".toString('base64'))}`;
+  //  const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data,"binary".toString('base64'))}`;
+
+   const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data,"binary").toString("base64")}`;
+
 
    // upload to image kit 
 
@@ -93,11 +104,17 @@ export const imageMessageController = async (req, res) => {
     const reply = { role : 'assistant',
       content : uploadResponse.url,
       timestamp: Date.now(),
-      isImage: false,
+      isImage: true,
+      isPublished
     };
 
+res.json({success: "true", reply})
+
+chat.messages.push(reply);
+await chat.save();
+await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
 
   } catch (error) {
-    
+    res.json({ success: false, message: error.message });
   }
 }
