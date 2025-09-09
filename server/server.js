8e2c93d00b071a7e5e1c38919f627a6394d69a1d
anuj9631 <anuj98426@gmail.com>
2025-09-09 17:03:07 +0530

    server issue

--- server/server.js
+++ server/server.js
@@ -5,22 +5,27 @@ import connectDB from './configs/db.js'
 import userRouter from './routes/userRoutes.js'
 import chatRouter from './routes/chatRoutes.js'
 import messageRouter from './routes/messageRoute.js'
-import serverless from 'serverless-http'
 
 const app = express()
 
 await connectDB()
 
-// Middleware
+
+
+//middleware
+
 app.use(cors())
 app.use(express.json())
 
-// Routes
-app.get('/', (req, res) => res.send('Server is Live!'))
+//routes
+app.get('/', (req,res)=> res.send('Server is Live!'))
+
 app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
-app.use('/api/message', messageRouter)
+app.use('/api/message/', messageRouter)
 
-// ❌ REMOVE app.listen()
-// ✅ Export handler for Vercel
-export const handler = serverless(app)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
