6f2ed994ae796b9246c10ca453ecef42a9fc445b
anuj9631 <anuj98426@gmail.com>
2025-09-11 16:46:18 +0530

    installed axios in fromtend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,73 +4,107 @@ import { dummyChats, dummyUserData } from "../assets/assets";
 import axios from 'axios';
 import toast from "react-hot-toast";
 
-axios.defaults.baseURL =  import.meta.env.VITE_SERVER_URL;
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
 const AppContext = createContext()
-export const AppContextProvider =({children}) =>{
+export const AppContextProvider = ({ children }) => {
 
-const navigate =useNavigate()
-const [user, setUser] = useState(null);
-const [chats, setChats] = useState([]);
-const [selectedChat, setSelectedChats] = useState(null);
-const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-const [token, setToken] = useState(localStorage.getItem('token') || null )
-const [loadingUser,setLoadingUser] = useState(true)
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChats] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const [loadingUser, setLoadingUser] = useState(true)
 
 
-const fetchUser = async () => {
-  try {
-   const {data} = await axios.get('/api/user/data',{headers: {Authorization: token}})
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get('/api/user/data', { headers: { Authorization: token } })
 
-   if (data.success){
-    setUser(data.user)
-   }else{
-     toast.error(data.message)
-   }
-  } catch (error) {
-    toast.error(error.message)
-  }finally{
-    setLoadingUser(false)
-  }
-}
-
-const fetchUserChats = async () => {
-  setChats(dummyChats)
-  setSelectedChats(dummyChats[0])
-}
-
-useEffect(()=>{
-  if(theme == 'dark'){
-    document.documentElement.classList.add('dark');
-  }else{
-    document.documentElement.classList.remove('dark');
-  }
-  localStorage.setItem('theme',theme)
-},[theme])
-
-useEffect(()=>{
-  if(user){
-    fetchUserChats()
-  }
-  else{
-    setChats([])
-    setSelectedChats(null)
+      if (data.success) {
+        setUser(data.user)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    } finally {
+      setLoadingUser(false)
+    }
   }
 
-},[user])
+  const createNewChat = async () => {
+    try {
+      if(!user) return toast('Login to create a new chat')
+      navigate('/')
+      await axios.get('/api/chat/create',{headers: {Authorization: token}})
+      await fetchUserChats()
 
-useEffect(()=>{
-  fetchUser()
-},[])
-  const value ={
-    navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChats, theme,setTheme
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
-  
-  return(
+
+
+
+  const fetchUserChats = async () => {
+    try {
+      const {data} = await axios.get('/api/chat/get',{headers: {Authorization: token}})
+      if(data.success){
+        setChats(data.chats)
+
+        if(data.chats.length === 0) {
+          await createNewChat();
+          return fetchUserChats()
+        } else {
+          setSelectedChats(data.chats[0])
+        }
+      }else {
+       toast.error(data.message) 
+      }
+    } catch (error) {
+      toast.error(error.message) 
+    }
+  }
+
+  useEffect(() => {
+    if (theme == 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  useEffect(() => {
+    if (user) {
+      fetchUserChats()
+    }
+    else {
+      setChats([])
+      setSelectedChats(null)
+    }
+
+  }, [user])
+
+  useEffect(() => {
+    if(token){
+        fetchUser()
+    } else {
+      setUser(null)
+      setLoadingUser(false)
+    }
+    
+  }, [])
+  const value = {
+    navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChats, theme, setTheme, createNewChat, loadingUser, fetchUserChats, token, setToken, axios }
+
+  return (
     <AppContext.Provider value={value}>
       {children}
     </AppContext.Provider>
   )
 }
 
-export const useAppContext = ()=> useContext(AppContext)
+export const useAppContext = () => useContext(AppContext)
