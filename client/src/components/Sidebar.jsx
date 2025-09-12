d8be047d00dc1d65d59d13e71e11d7def59a1955
anuj9631 <anuj98426@gmail.com>
2025-09-12 17:10:48 +0530

    error fixed

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -112,7 +112,7 @@ const Sidebar = ({isMenuOpen, setIsMenuOpen}) => {
       {/* Community*/}
       <div
         onClick={() => {
-          navigate("/Community"); setIsMenuOpen(false)
+          navigate("/community"); setIsMenuOpen(false)
         }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
